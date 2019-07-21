2caac5c76dc39deaedfe9ebedad1ba3f12420cfc
Shiv-Shanakr-pd <bcashivignou@gmail.com>
2019-07-21 18:06:50 +0530

    Initial Commit

---
+++ MvcPractice/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcPractice.Models;
+using MvcPractice.ViewModels;
+
+namespace MvcPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+        private IHostingEnvironment _hostingEnvironment;
+        public HomeController(IEmployeeRepository employeeRepository, IHostingEnvironment hostingEnvironment)
+        {
+            _employeeRepository = employeeRepository;
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployees();
+            return View(model);
+        }
+        public ViewResult Details(int? id)
+        {
+            EmployeeViewModel employeeViewModel = new EmployeeViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(id ?? 1),
+                PageTitle = "Employee Data"
+            };
+            return View(employeeViewModel);
+        }
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreateEmployeeViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                string uniqueFileName = null;
+                if(model.Photos != null && model.Photos.Count > 0)
+                {
+                    foreach(IFormFile photo in model.Photos)
+                    {
+                        string UploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "UploadedEmployeePhotos");
+                        uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+                        string FilePath = Path.Combine(UploadsFolder, uniqueFileName);
+                        photo.CopyTo(new FileStream(FilePath, FileMode.Create));
+                    }                    
+                }
+                Employee newEmployee = new Employee()
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Department = model.Department,
+                    PhotoPath = uniqueFileName
+                };
+                _employeeRepository.CreateEmployee(newEmployee);    
+                return RedirectToAction("Details", new { id = newEmployee.Id });
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MvcPractice/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MvcPractice.Models;
+namespace MvcPractice
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+        public Startup(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
+            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+           
+
+            app.UseStaticFiles();
+            
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
